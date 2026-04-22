3aef3764417b4ba575b78291fd6f15eb420d54cb
stanie12 <stanie.junior.kiadjeu@tha.de>
2026-04-22 16:34:46 +0200

    Ich bin heute mit dem Kontrollfluss fertig

--- src/kontollstrukturen/BMI.java
+++ src/kontollstrukturen/BMI.java
@@ -12,7 +12,32 @@ public class BMI {
 		
 		System.out.println("Geben  sie Ihr Gewicht hier ein : ");
 		
-		 double ge
+		 double gewicht = scan.nextDouble();
+		 
+		 System.out.println("Geben sie Ihre Groesse hier ein : ");
+		 
+		  double  groesse = scan.nextDouble();
+		  
+		  double bmi = gewicht/ (groesse  *groesse);
+		  
+		  System.out.println("Ihr BMI ist : " +bmi);
+		  
+		  if (bmi<18.5) {
+			
+			  System.out.println("Sie sind Untergewicht .");
+			  
+		}else if (bmi >= 18.5 && bmi <25) {
+			
+			System.out.println("Sie sind Normalgewicht");
+			
+		}else if(bmi >=25 && bmi <30) {
+			
+			System.out.println( "Sie sind Übergewicht");
+			
+		} else if(bmi >=30) {
+			
+			System.out.println("Sie sind Adipositas");
+		}
 		
 	}
 }
